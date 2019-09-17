b41e3469d5d98da4ab5fc586eaca1952d35efa4f
andrei <andrei@100danish.com>
2019-09-17 23:30:20 +0300

    Update Build

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
 TAG=$(sed -e "s/refs\/tags\///g" <<< $GITHUB_REF)
 
 # Replace the version in these 2 files.
-sed -i -e "s/__STABLE_TAG__/$TAG/g" ./package.json
+sed -i -e "s/\"version\": \"(.*)\"/\"version\": \"$TAG\"/g" ./package.json
